6c8f9eb1dbc84edcce62c2965a29a9954066eb62
RyanWood18 <32538207+RyanWood18@users.noreply.github.com>
2022-01-04 11:34:37 +0000

    Dev fixes

--- src/Broker/QuotationNotifier.cs
+++ src/Broker/QuotationNotifier.cs
@@ -7,10 +7,12 @@ namespace Broker;
 public class QuotationNotifier
 {
     private readonly SendGridClient _emailClient;
+    private readonly IConfiguration _config;
 
-    public QuotationNotifier(SendGridClient emailClient)
+    public QuotationNotifier(SendGridClient emailClient, IConfiguration config)
     {
-        this._emailClient = emailClient;
+        _emailClient = emailClient;
+        _config = config;
     }
 
     public async Task Notify(CompletedRequest request)
@@ -19,12 +21,12 @@ public class QuotationNotifier
         await _emailClient.SendEmailAsync(message);
     }
     
-    private static SendGridMessage CreateMessage(string toAddress, List<Quotation> quotes)
+    private SendGridMessage CreateMessage(string toAddress, List<Quotation> quotes)
     {
         var message = new SendGridMessage();
         message.AddTo(toAddress);
         message.SetSubject(quotes.Count > 0 ? "Quotes Received" : "An update on your loan quote request");
-        message.SetFrom(Environment.GetEnvironmentVariable("FromEmailAddress"));
+        message.SetFrom(_config["FromEmailAddress"]);
         message.AddContent("text/html", EmailBuilder.BuildEmail(quotes));
         return message;
     }
